c5a797cbd9fec910d6403ae8d23b5ca6743b383c
Robbie Rankin <robbierankin15@gmail.com>
2025-12-21 21:26:48 -0500

    firestore setup finish

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAly4kSQgSYyuSi3mq4F3JCJbyJkVSmNkM",
+    authDomain: "mom-xmas.firebaseapp.com",
+    projectId: "mom-xmas",
+    storageBucket: "mom-xmas.firebasestorage.app",
+    messagingSenderId: "17129859812",
+    appId: "1:17129859812:web:dd921a06023bd2cf52daf1",
+    measurementId: "G-K5LSNK2GE8"
+  }
+}
