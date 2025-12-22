316ee196a597db0afe4d32219e54520dd260b579
Robbie Rankin <robbierankin15@gmail.com>
2025-12-21 22:51:00 -0500

    merge

--- src/app/components/game-page/game-page.component.ts
+++ src/app/components/game-page/game-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, EventEmitter, HostListener, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {GameService} from '../../services/game.service';
 import {BoardComponent} from '../board/board.component';
